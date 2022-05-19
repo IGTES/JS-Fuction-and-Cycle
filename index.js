dad107c91b0aeda8ef619921653ad81b534fa7a6
IGTES <itias89@gmail.com>
2022-05-19 21:12:37 +0300

    initial

---
+++ index.js
@@ -0,0 +1,54 @@
+
+/*1
+let name1 = 11;
+let name2 = "true";
+let name3 = "java script";
+let name4 = 100;
+console.log(name1,name2,name3,name4);
+*/
+
+
+/*2
+let a1 = 'Hello', a2 = 'userName'
+let r = prompt('Как Вас зовут?');
+    if(r = a2) {
+    alert ('Hello, Vasya!');
+}
+*/
+
+
+/*3
+let min = 15;
+let r = prompt('Введите количество минут');
+if (r >= 0 && r <= 15) {
+	alert('В первую четверть.');
+}
+else if (r >= 15 && r <= 30) {
+	alert('Во вторую четверть.');
+}
+else if (r >= 30 && r <= 45) {
+	alert('В третью четверть.');
+}
+else if (r >= 45 && r <= 60) {
+	alert('В четвертую четверть.');
+}
+*/
+
+
+/*4
+let num;
+for(num = 10; num <= 50; num ++){
+    if(num %5 == 0){
+        console.log (num)
+    }
+}
+*/
+
+
+
+/*5
+let mult = 1;
+for(let i=5; i<=12; i++){
+    mult *= i;
+}console.log(mult)
+*/
